e477ac43f7a21824a10812f2499e6a614b47114a
Kuri148 <117333210+Kuri148@users.noreply.github.com>
2023-04-28 12:49:34 +0900

    Hoping this is the way

---
+++ MemoryGameENJP/Assets/UdonSharp 1/CardPlacement.cs
@@ -0,0 +1,98 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CardPlacement : UdonSharpBehaviour
+{
+    GameObject[] cardDeck = new GameObject[16];
+    Vector3[] cardTransforms = new Vector3[16];
+    public GameObject cardBox;
+    
+    void Start()
+    {
+        BuildInitialDeck();
+        BuildInitialTransforms();
+        PlaceCardsOnTransforms();
+    }
+    private void BuildInitialDeck()
+    {
+        for (int i = 0; i < cardBox.transform.childCount; i++)
+        {
+            GameObject card = cardBox.transform.GetChild(i).gameObject;
+            cardDeck[i] = card;
+        }
+    }
+    private void BuildInitialTransforms()
+    {
+        int x = -8;
+        int y = 4;
+        new Vector3 (-8, 4, 0);
+        for (int i = 0; i < 16; i++)
+        {
+            if (y == 4)
+            {
+                Vector3 cardPosition = new Vector3 (x, y, 0);
+                cardTransforms[i] = cardPosition;
+                x += 4;
+                if (x == 12)
+                {
+                    x = -8;
+                    y -= 2;
+                }
+            }
+            if (y == 2)
+            {
+                Vector3 cardPosition = new Vector3 (x, y, 0);
+                cardTransforms[i] = cardPosition;
+                x += 4;
+                if (x == 12)
+                {
+                    x = -8;
+                    y -= 2;
+                }
+            }
+            if (y == 0)
+            {
+                Vector3 cardPosition = new Vector3 (x, y, 0);
+                cardTransforms[i] = cardPosition;
+                x += 4;
+                if (x == 12)
+                {
+                    x = -8;
+                    y -= 2;
+                }
+            }
+            if (y == -2)
+            {
+                Vector3 cardPosition = new Vector3 (x, y, 0);
+                cardTransforms[i] = cardPosition;
+                x += 4;
+                if (x == 12)
+                {
+                    x = -8;
+                    y -= 2;
+                }
+            }
+            if (y == -4)
+            {
+                Vector3 cardPosition = new Vector3 (x, y, 0);
+                cardTransforms[i] = cardPosition;
+                x += 4;
+                if (x == 2)
+                {
+                    x = -8;
+                    y -= 2;
+                }
+            }
+        }
+    }
+    private void PlaceCardsOnTransforms()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            cardDeck[i].transform.localPosition = cardTransforms[i];
+        }
+    }
+}
